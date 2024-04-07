e68908981a271a09e13e7288ef845a0fc518a3c1
kisinga <JohnMutemi>
2024-04-07 20:39:00 +0300

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const userDetails = {
+        name: userName,
+        email: userEmail
+    };
+    
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userDetails),
+    };
+    
+    // Return the fetch chain here
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(response => response.json())
+        .then(data => {
+            const id = data.id;
+            const element = document.createElement("div");
+            document.body.append(element);
+            element.append(id);
+        })
+        
+        .catch(error => {
+            // Catching and handling errors
+            const errorMessage = document.createElement("div");
+             // Displaying the error message
+            errorMessage.textContent = error.message;
+            document.body.appendChild(errorMessage);
+        });
+        
+}
